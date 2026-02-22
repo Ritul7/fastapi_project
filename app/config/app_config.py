731b3c0904606b6d1f7edc788d93cd2d87d25986
Ritul7 <sharma.ritul1601@gmail.com>
2026-02-22 13:07:17 +0530

    Configured db

---
+++ app/config/app_config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from functools import lru_cache
+
+class AppConfig(BaseSettings):
+
+    app_name:str = "FASTAPI_PROJECT"
+    app_env:str = "development"
+    database_url:str
+
+    model_config = SettingsConfigDict(env_file=".env")          #This line tells pydantic to read env variables form the .env file
+
+
+@lru_cache
+def getAppConfig():
+    return AppConfig()              # everytime it is called, it reads .env file, so intead of reading evrytime, lru_cache helps in giving the stored result
+
+
+
+
+
+# BaseSettings -> Tells pydantic to automatically read env variables and .env file into python class and allows centralized configuration management
+# functools... lru cache -> Caches the result of a function so it is not recreated agian n again
--- app/main.py
+++ app/main.py
@@ -3,9 +3,14 @@ from typing import Annotated
 from app.routes import todo                 # app ke andr routes package tha, uske andr todo file thi
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
+from dotenv import load_dotenv
+import os
 
 
 app = FastAPI()
+
+load_dotenv()                           # Isse .env ki chize access ho payengi yha pr (It is one of the method, but a good methos is to go with config folder through pydantic)
+
 
 app.include_router(todo.router)             # Telling our app that we've created routes there
 
@@ -23,14 +28,17 @@ async def validation_exception_handler(request, exc):
 
 
 
-# @app.get('/')
-# def root():
-#     return {"Message": "Everything is running"}
-
 @app.get('/')
-def get_name(request: Request):
-    params = request.query_params
-    return {"Message": params }
+def root():
+    return {
+        "Message": "Everything is running",
+        "App name": os.getenv("APP_NAME")
+    }
+
+# @app.get('/')
+# def get_name(request: Request):
+#     params = request.query_params
+#     return {"Message": params }
 
 @app.get('/items/{id}')
 def get_items(id: Annotated[int, Path(ge=0)]):
